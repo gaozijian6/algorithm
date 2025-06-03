793543d27a43f7db6413f34961eb2d6cda264386
高高子健 <gaogaozijian@gaogaozijiandeMac-mini.local>
2025-06-03 21:12:44 +0800

    1

--- interview2.md
+++ interview2.md
@@ -4,3 +4,9 @@ webpack是一个现代js的静态打包器，他会递归的构建依赖关系�
 输出（output）：告诉webpack将打包后的文件输出到哪里，默认是dist/index.js
 加载器（loader）：webpack本身只能理解js和json文件，加载器让webpack能处理其他类型的文件
 插件（plugin）：
+babel-loader做js、ts、jsx、tsx转换
+css-loader做css转换
+ts-loader：专门的ts转换，相比babel-loader在构建时能做额外的类型检查
+
+### vite：
+下一代的构建工具，启动速度很快，基于ESM的冷启动，按需编译，利用浏览器缓存策略，开发环境是esbuild在转换，生产环境使用Rollup打包
---
+++ 自定义hook请求锁.tsx
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { useCallback, useRef, useState } from "react";
+import { Button } from "antd";
+
+function App() {
+  function useRequestLock(requestFn: (...args: any[]) => Promise<any>) {
+    const [loading, setLoading] = useState(false);
+    const lockRef = useRef(false);
+
+    const execute = useCallback(
+      async (...args: any[]) => {
+        if (lockRef.current) {
+          return;
+        }
+
+        try {
+          lockRef.current = true;
+          setLoading(true);
+          const result = await requestFn(...args);
+          return result;
+        } finally {
+          lockRef.current = false;
+          setLoading(false);
+        }
+      },
+      [requestFn]
+    );
+
+    return { loading, execute };
+  }
+  async function request() {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return axios.post("http://127.0.0.1:3000/api/v1/test");
+  }
+  const { loading, execute } = useRequestLock(request);
+  return (
+    <div>
+      <Button onClick={() => execute()} loading={loading}>
+        click
+      </Button>
+    </div>
+  );
+}
+
+export default App;
